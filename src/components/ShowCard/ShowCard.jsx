b5e94015eed2b150fbb8b8266c7738ed5d8346ce
siddiquenaim <naim.niter.tex06@gmail.com>
2023-06-02 16:12:28 +0600

    fixed responsiveness

--- src/components/ShowCard/ShowCard.jsx
+++ src/components/ShowCard/ShowCard.jsx
@@ -5,21 +5,16 @@ import { Link } from "react-router-dom";
 const ShowCard = ({ movie }) => {
   const { show } = movie;
   return (
-    <Card className="col-md-3 bg-white p-0" style={{ width: "full" }}>
+    <Card
+      className="col-md-4 col-lg-3 bg-white p-0 mx-auto"
+      style={{ width: "full" }}
+    >
       <Card.Img variant="top" src={show.image.medium} />
       <Card.Body className="p-4">
         <div className="mb-3">
           <Card.Title>{show.name}</Card.Title>
-          <Card.Text>
-            {show.genres.map((genre, i) => (
-              <span
-                className="px-1 py-1 rounded bg-danger me-2 text-white fs-6"
-                key={i}
-              >
-                {genre}
-              </span>
-            ))}
-          </Card.Text>
+
+          <Card.Text>{show?.genres[0]}</Card.Text>
         </div>
         <Card.Text>
           Language: <span className="fw-bold">{show?.rating?.average}</span>
